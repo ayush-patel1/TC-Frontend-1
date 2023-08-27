01669587ee6b832510c07b9a093cb491931d95ad
Mayank <mayanksahu1005@gmail.com>
2023-08-27 22:17:26 +0530

    Journey page created, First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Homepage from "./pages/Homepage"
-import Aavartan from "./pages/Aavartan"
-import Vigyaan from "./pages/Vigyaan"
-import Navbar from "./components/Navbar"
-import Footer from "./components/Footer"
+import Homepage from "./pages/Homepage";
+import Aavartan from "./pages/Aavartan";
+import Vigyaan from "./pages/Vigyaan";
+import Journey from "./sections/OurJourney";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div>
       <BrowserRouter>
-      <Navbar/>
+        <Navbar />
         <Routes>
-          <Route exact path="/" element={<Homepage/>} />
+          <Route exact path="/" element={<Homepage />} />
           <Route exact path="/aavartan" element={<Aavartan />} />
           <Route exact path="/vigyaan" element={<Vigyaan />} />
+          <Route exact path="/journey" element={<Journey />} />
           {/* <Route exact path="/events" element={<EventPage />} /> */}
         </Routes>
-        <Footer/>
+        <Footer />
       </BrowserRouter>
     </div>
   );

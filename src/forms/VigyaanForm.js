7df274208b0cb79989a28d47a6505bdea959f8ec
Piyush-onGIT <thisissahupiyush@gmail.com>
2023-09-01 00:33:54 +0530

    vigyaan regn form backend done

--- src/forms/VigyaanForm.js
+++ src/forms/VigyaanForm.js
@@ -1,8 +1,35 @@
+import axios from "axios";
 import { React, useEffect, useState } from "react";
 import { connect } from "react-redux";
+import urls from "../urls.json"
+
+const backend = urls.backend
 
 const VigyaanForm = () => {
   const [memberCount, setMemberCount] = useState(0);
+  const [form, set] = useState({
+    Team_name: "",
+    Leader_name: "",
+    Leader_email: "",
+    Leader_whatsapp: "",
+    College: "",
+    Leader_branch: "",
+    Leader_yog: "",
+    Member2_name: "",
+    Member2_email: "",
+    Member2_yog: "",
+    Member2_whatsapp: "",
+    Member2_branch: "",
+    Member3_name: "",
+    Member3_email: "",
+    Member3_yog: "",
+    Member3_whatsapp: "",
+    Member3_branch: "",
+    Problem_code: "",
+    file: null,
+  });
+  const [uploadedFileName, setUploadedFileName] = useState("");
+  const [isSubmitting, setSubmit] = useState(false);
 
   const handleAddMember = () => {
     if (memberCount < 2) {
@@ -16,6 +43,55 @@ const VigyaanForm = () => {
     }
   };
 
+  const handle = (e) => {
+    const update = { ...form }
+    update[e.target.name] = e.target.value
+    set(update)
+  }
+
+  const submit = async () => {
+    setSubmit(true)
+    console.log(form)
+    if (memberCount < 1) {
+      alert("Minimum Team Size: 2")
+    }
+    else {
+      let condition1 = form.Team_name !== "" && form.Leader_name !== "" && form.Leader_email !== "" && form.Leader_whatsapp !== "" && form.College !== "" && form.Leader_yog !== "" && form.Leader_branch !== "" && form.Member2_name !== "" && form.Member2_email !== "" && form.Member2_yog !== "" && form.Member2_whatsapp !== "" && form.Member2_branch !== "" && form.Problem_code !== "" && form.file;
+
+      let condition2 = true
+      if (memberCount == 2) condition2 = form.Member3_email !== "" && form.Member3_name !== "" && form.Member3_whatsapp !== "" && form.Member3_whatsapp !== "" && form.Member3_yog !== "" && form.Member3_branch !== "";
+
+      if (condition1 && condition2) {
+        try {
+          const res = await axios.post(`${backend}/vigyaanReg`, form, {
+            headers: {
+              "Content-Type": "multipart/form-data",
+            }
+          })
+          alert(res.data.message)
+        }
+        catch (err) {
+          console.error(err)
+          alert(err.reponse.data.message)
+        }
+      }
+      else {
+        alert("Please fill all the necessary details")
+      }
+    }
+    setSubmit(false)
+  }
+
+  const handleFileChange = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      const newData = { ...form };
+      newData["file"] = file;
+      set(newData);
+      setUploadedFileName(file.name);
+    }
+  };
+
   const renderMemberFields = () => {
     const members = [];
     for (let i = 1; i <= memberCount; i++) {
@@ -23,16 +99,47 @@ const VigyaanForm = () => {
         <div key={i}>
           <li>
             <input
+              name={`Member${i + 1}_name`}
               className="memberName"
               type="text"
               placeholder={`Member ${i} Name`}
+              onChange={(e) => handle(e)}
             />
           </li>
           <li>
             <input
+              name={`Member${i + 1}_whatsapp`}
               className="memberNumber"
               type="text"
               placeholder={`Member ${i} Whatsapp Number`}
+              onChange={(e) => handle(e)}
+            />
+          </li>
+          <li>
+            <input
+              name={`Member${i + 1}_email`}
+              className="memberName"
+              type="text"
+              placeholder={`Member ${i} Email ID`}
+              onChange={(e) => handle(e)}
+            />
+          </li>
+          <li>
+            <input
+              name={`Member${i + 1}_branch`}
+              className="memberName"
+              type="text"
+              placeholder={`Member ${i}'s Branch`}
+              onChange={(e) => handle(e)}
+            />
+          </li>
+          <li>
+            <input
+              name={`Member${i + 1}_yog`}
+              className="memberName"
+              type="text"
+              placeholder={`Member ${i}'s year of graduation`}
+              onChange={(e) => handle(e)}
             />
           </li>
         </div>
@@ -43,7 +150,7 @@ const VigyaanForm = () => {
 
   return (
     <div className="metaportal_fn_mintpage">
-      <div className="container small" style={{paddingTop:"3rem"}}>
+      <div className="container small" style={{ paddingTop: "3rem" }}>
         <div className="metaportal_fn_mintbox">
           <div className="mint_left">
             <div className="mint_title">
@@ -52,41 +159,76 @@ const VigyaanForm = () => {
             <div className="mint_list">
               <ul>
                 <li>
-                  <input id="teamName" type="text" placeholder="Team Name" />
+                  <input name="Team_name" id="teamName" type="text" placeholder="Team Name" onChange={(e) => handle(e)} />
                 </li>
                 <li>
                   <input
                     id="leaderName"
                     type="text"
+                    name="Problem_code"
+                    placeholder="Problem Code"
+                    onChange={(e) => handle(e)}
+                  />
+                </li>
+                <li>
+                  <input
+                    id="leaderName"
+                    type="text"
+                    name="Leader_name"
                     placeholder="Your Leader Name"
+                    onChange={(e) => handle(e)}
                   />
                 </li>
                 <li>
                   <input
                     id="leaderNumber"
                     type="text"
+                    name="Leader_whatsapp"
                     placeholder="Your Leader Whatsapp Number"
+                    onChange={(e) => handle(e)}
                   />
                 </li>
                 <li>
                   <input
                     id="leaderEmail"
                     type="text"
+                    name="Leader_email"
                     placeholder="Your Leader Email ID"
+                    onChange={(e) => handle(e)}
                   />
                 </li>
-                <li>
+                {/* <li>
                   <input
                     id="teamSize"
                     type="text"
                     placeholder="Team Size (Max 3)"
                   />
-                </li>
+                </li> */}
                 <li>
                   <input
+                    name="College"
                     id="collegeName"
                     type="text"
                     placeholder="College Name"
+                    onChange={(e) => handle(e)}
+                  />
+                </li>
+                <li>
+                  <input
+                    name="Leader_branch"
+                    id="leaderBranch"
+                    type="text"
+                    placeholder="Leader Branch"
+                    onChange={(e) => handle(e)}
+                  />
+                </li>
+                <li>
+                  <input
+                    name="Leader_yog"
+                    id="leaderYog"
+                    type="text"
+                    placeholder="Leader's year of graduation"
+                    onChange={(e) => handle(e)}
                   />
                 </li>
                 {renderMemberFields()}
@@ -95,12 +237,11 @@ const VigyaanForm = () => {
                     padding: "0",
                     display: "flex",
                     justifyContent: "space-between",
-                    margin:"0"
+                    margin: "0"
                   }}
                 >
                   {memberCount < 2 && (
                     <li className="add-remove" onClick={handleAddMember}>
-                      {" "}
                       + Add Member
                     </li>
                   )}
@@ -116,35 +257,30 @@ const VigyaanForm = () => {
                 style={{ display: "none" }}
                 id="file-input"
                 type="file"
-                // onChange={handleFileChange}
+                onChange={handleFileChange}
               />
-              <label style={{display:"flex", justifyContent:"center", flexDirection:"column"}} htmlFor="file-input">
-                <a
-                  href="#"
-                  target="_blank"
-                  rel="noreferrer"
-                  className="metaportal_fn_button_2"
-                  // onClick={submit}
-                  // disabled={isSubmitting}
-                >
-                  <span>Upload Your Abstract</span>
-                </a>
-                {/* {uploadedFileName && (
-                        <p style={{ color: "white", paddingTop: "1rem" }}>Uploaded File: {uploadedFileName}</p>
-                      )} */}
+              <label style={{ display: "flex", justifyContent: "center", flexDirection: "column" }} htmlFor="file-input">
+                <span className="metaportal_fn_button_2">Upload Your Abstract</span>
+                {uploadedFileName && (
+                  <p style={{ color: "white", paddingTop: "1rem" }}>Uploaded File: {uploadedFileName}</p>
+                )}
               </label>
             </div>
-            <div className="mint_desc" style={{paddingTop:"4rem",}}>
-              <a
-                href="#"
-                target="_blank"
-                rel="noreferrer"
-                className="metaportal_fn_button"
-                // onClick={submit}
-                // disabled={isSubmitting}
-              >
-                <span>Submit</span>
-              </a>
+            <div className="mint_desc" style={{ paddingTop: "4rem", }}>
+              {!isSubmitting ?
+                <div
+                  target="_blank"
+                  rel="noreferrer"
+                  className="metaportal_fn_button"
+                  style={{ cursor: "pointer" }}
+                  onClick={submit}
+                  disabled={isSubmitting}
+                >
+                  <span>Submit</span>
+                </div>
+                :
+                <>Submitting...</>
+              }
               <p>* Read the Rules & Regulations before Submitting</p>
             </div>
           </div>
@@ -206,34 +342,34 @@ const VigyaanForm = () => {
           </div>
         </div>
         <div className="metaportal_fn_nft_cats">
-            <ul>
-              <li>
-                <div className="item">
-                  <h4 className="parent_category">10th September</h4>
-                  <h3 className="child_category" title="Black Yukata">
+          <ul>
+            <li>
+              <div className="item">
+                <h4 className="parent_category">10th September</h4>
+                <h3 className="child_category" title="Black Yukata">
                   Registration and Abstract Submission
-                  </h3>
-                </div>
-              </li>
-              <li>
-                <div className="item">
-                  <h4 className="parent_category">7th & 28th September</h4>
-                  <h3 className="child_category" title="Daydreaming">
+                </h3>
+              </div>
+            </li>
+            <li>
+              <div className="item">
+                <h4 className="parent_category">7th & 28th September</h4>
+                <h3 className="child_category" title="Daydreaming">
                   Presentation Round
-                  </h3>
-                </div>
-              </li>
-              <li>
-                <div className="item">
-                  <h4 className="parent_category">23rd October(Tentative)</h4>
-                  <h3 className="child_category" title="Fireflies, Smoke">
+                </h3>
+              </div>
+            </li>
+            <li>
+              <div className="item">
+                <h4 className="parent_category">23rd October(Tentative)</h4>
+                <h3 className="child_category" title="Fireflies, Smoke">
                   Prototype Round
-                  </h3>
-                </div>
-              </li>
+                </h3>
+              </div>
+            </li>
 
-            </ul>
-          </div>
+          </ul>
+        </div>
       </div>
     </div>
   );

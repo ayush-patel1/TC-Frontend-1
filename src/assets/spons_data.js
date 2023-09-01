7dbdb7077ee0acb5430959eef3c000d3ef8a26eb
Vibhuti <vibhuti6378@gmail.com>
2023-09-01 22:29:54 +0530

    Spons section done

---
+++ src/assets/spons_data.js
@@ -0,0 +1,100 @@
+import img_1 from '../assets/spons_images/Bonzo_page-0001.jpg';
+import img_2 from '../assets/spons_images/CSPDCL.png';
+import img_3 from '../assets/spons_images/DABANG.jpeg';
+import img_4 from '../assets/spons_images/MCC New Logo - White.png';
+import img_5 from '../assets/spons_images/ONGO-BLUE.png';
+import img_6 from '../assets/spons_images/PAUGAM.jpeg';
+import img_7 from '../assets/spons_images/PicsArt_02-24-08.57.48.jpg';
+import img_8 from '../assets/spons_images/SPOT.jpeg';
+import img_9 from '../assets/spons_images/Saurum.png';
+import img_10 from '../assets/spons_images/WhatsApp Image 2023-02-02 at 9.08.32 PM.jpeg';
+import img_11 from '../assets/spons_images/cbc.jpg';
+import img_12 from '../assets/spons_images/comp-removebg-preview.png';
+import img_13 from '../assets/spons_images/grand news.png';
+import img_14 from '../assets/spons_images/hitavada bg.png';
+import img_15 from '../assets/spons_images/myfm logo (1).png';
+import img_16 from '../assets/spons_images/rahul_travels-removebg-preview.png';
+
+const Spons_data = [
+    {
+        id: 1,
+        dis: "img1",
+        url: img_1,
+        
+    },
+    {
+        id: 2,
+        dis: "img2",
+        url: img_2,
+    },
+    {
+        id: 3,
+        dis: "img3",
+        url: img_3,
+    },
+    {
+        id: 4,
+        dis: "img4",
+        url: img_4,
+    },
+    {
+        id: 5,
+        dis: "img5",
+        url: img_5,
+    },
+    {
+        id: 6,
+        dis: "img6",
+        url: img_6,
+    },
+    {
+        id: 7,
+        dis: "img7",
+        url: img_7,
+    }, {
+        id: 8,
+        dis: "img8",
+        url: img_8,
+    }, {
+        id: 9,
+        dis: "img9",
+        url: img_9,
+    },
+    {
+        id: 10,
+        dis: "img10",
+        url: img_10,
+    },
+    {
+        id: 11,
+        dis: "img11",
+        url: img_11,
+    },
+    {
+        id: 12,
+        dis: "img12",
+        url: img_12,
+    },
+    {
+        id: 13,
+        dis: "img13",
+        url: img_13,
+    },
+    {
+        id: 14,
+        dis: "img14",
+        url: img_14,
+    },
+    {
+        id: 15,
+        dis: "img15",
+        url: img_15,
+    },
+    {
+        id: 16,
+        dis: "img16",
+        url: img_16,
+    },
+];
+
+export default Spons_data;

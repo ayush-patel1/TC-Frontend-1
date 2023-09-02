ba1ce4d3ff827fd29aa221ee3c9f148fa5151a05
ankitlakra <ankitlakra157@gmail.com>
2023-09-02 23:11:29 +0530

    loader

--- src/pages/Aavartan.js
+++ src/pages/Aavartan.js
@@ -1,25 +1,35 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 import Events from '../sections/Events'
 import EveningShow from '../sections/EveningShow'
 import EventsGallery from '../sections/EventsGallery'
 import AavartanHero from '../sections/hero/AavartanHero'
-
+import FullPageLoader from '../layout/FullPageLoader';
 const Aavartan = () => {
+  const [isLoading, setIsLoading] = useState(true);
   useEffect(() => {
-    AOS.init({ duration: 1000 });
-    window.scrollTo(0,0)
+    AOS.init({ duration: 1000, });
+    window.scrollTo(0, 0)
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 2000);
   }, [])
   return (
     <div>
-      {/* <Events/> */}
-      {/* <AavartanHero/> */}
-      <AavartanHero/>
-      <div className='AavartanContainer'>
-      <EveningShow/>
-      <EventsGallery/>
-      </div>
+      {isLoading ? (
+        <FullPageLoader />
+      ) : (
+        <div>
+          {/* <Events/> */}
+          {/* <AavartanHero/> */}
+          <AavartanHero />
+          <div className='AavartanContainer'>
+            <EveningShow />
+            <EventsGallery />
+          </div>
+        </div>
+      )}
     </div>
   )
 }
--- src/pages/Vigyaan.js
+++ src/pages/Vigyaan.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect,useState } from 'react'
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 import VigyaanInfo from '../sections/VigyaanInfo'
@@ -6,20 +6,31 @@ import VigyaanGallery from '../sections/VigyaanGallery'
 import VigyaanForm from '../forms/VigyaanForm'
 import ProblemStatement from '../sections/ProblemStatement'
 import VigyaanHero from '../sections/hero/VigyaanHero'
+import FullPageLoader from '../layout/FullPageLoader';
 const Vigyaan = () => {
+  const [isLoading, setIsLoading] = useState(true);
   useEffect(() => {
-    AOS.init({ duration: 1000 });
-    window.scrollTo(0,0)
+    AOS.init({ duration: 1000, });
+    window.scrollTo(0, 0)
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 2000);
   }, [])
   return (
     <div>
-      {/* <VigyaanInfo/> */}
-      <VigyaanHero/>
-      <div className="AavartanContainer">
-        <VigyaanForm/>
-        <ProblemStatement/>
-      <VigyaanGallery/>
-      </div>
+      {isLoading ? (
+        <FullPageLoader />
+      ) : (
+        <div>
+          {/* <VigyaanInfo/> */}
+          <VigyaanHero />
+          <div className="AavartanContainer">
+            <VigyaanForm />
+            <ProblemStatement />
+            <VigyaanGallery />
+          </div>
+        </div>
+      )}
     </div>
   )
 }

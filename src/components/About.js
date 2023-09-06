3238855e00c908f14b7b6eff33eeb310ce033b6e
Aniket Kumar <kraniket123654@gmail.com>
2023-09-06 20:14:03 +0530

    Bug4

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,11 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import styles from "./About.module.css";
 import Title from "./Title";
+import CountUp from "react-countup";
+import ScrollTrigger from "react-scroll-trigger";
 
 function About() {
+  const [counterOn, setCounterOn] = useState(false);
   return (
     <div className={styles.about_container}>
       <Title color={"ABOUT"} noncolor={"US"} />
@@ -17,7 +20,7 @@ function About() {
           </p>
         </div>
       </div>
-
+      <ScrollTrigger style={{width:"100%"}} onEnter={()=> setCounterOn(true)} onExit={()=> setCounterOn(false)}>
           <div className={styles.about_boxes}>
             <div className={styles.about_box}>
               <img
@@ -25,7 +28,7 @@ function About() {
                 src="https://www.tryst-iitd.org/static/media/about-5.973f96e28be79bfad0f1c0b987f3ab85.svg"
                 alt="icon"
               />
-              <div className={styles.abox_num}>10,000 +</div>
+              <div className={styles.abox_num}> {counterOn && <CountUp start={9000} end={10000} duration={1.5} delay={0}/>} +</div>
               <div className={styles.abox_Title}>FOOTFALL</div>
             </div>
             <div className={styles.about_box}>
@@ -34,7 +37,7 @@ function About() {
                 src="https://www.tryst-iitd.org/static/media/about-4.9b0a31e0bb61d4ed5a53c19e6379b710.svg"
                 alt="icon"
               />
-              <div className={styles.abox_num}>50 +</div>
+              <div className={styles.abox_num}> {counterOn && <CountUp start={0} end={50} duration={1.5} delay={0}/>} +</div>
               <div className={styles.abox_Title}>COLLEGES</div>
             </div>
             <div className={styles.about_box}>
@@ -43,7 +46,7 @@ function About() {
                 src="https://www.tryst-iitd.org/static/media/about-3.d19bb113f00f387d3f16f4558b746bc7.svg"
                 alt="icon"
               />
-              <div className={styles.abox_num}>30 +</div>
+              <div className={styles.abox_num}> {counterOn && <CountUp start={0} end={30} duration={1.5} delay={0}/>} +</div>
               <div className={styles.abox_Title}>EVENTS</div>
             </div>
             <div className={styles.about_box}>
@@ -52,7 +55,7 @@ function About() {
                 src="https://www.tryst-iitd.org/static/media/about-1.942f425aee9ada78dbb276060beebec2.svg"
                 alt="icon"
               />
-              <div className={styles.abox_num}>6 Lacs +</div>
+              <div className={styles.abox_num}> {counterOn && <CountUp start={0} end={6} duration={1.5} delay={0}/>} Lacs +</div>
               <div className={styles.abox_Title}>PRIZES</div>
             </div>
             <div className={styles.about_box}>
@@ -61,10 +64,11 @@ function About() {
                 src="https://www.tryst-iitd.org/static/media/about-2.0662e7d746094da22acfb69f414a508b.svg"
                 alt="icon"
               />
-              <div className={styles.abox_num}>200 +</div>
+              <div className={styles.abox_num}> {counterOn && <CountUp start={0} end={200} duration={1.5} delay={0}/>} +</div>
               <div className={styles.abox_Title}>PARTICIPANTS</div>
             </div>
       </div>
+      </ScrollTrigger>
     </div>
   );
 }

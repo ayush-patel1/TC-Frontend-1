825d1f6cd9b7705a76706163139fcde9df5e18f7
Aniket Kumar <kraniket123654@gmail.com>
2023-09-07 19:16:21 +0530

    removed blur

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux'; // Import the Provider component
 import store from './redux/store'; // Import your Redux store
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import StarFieldAnimation from './layout/StarFieldAnimation';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -19,4 +19,3 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();

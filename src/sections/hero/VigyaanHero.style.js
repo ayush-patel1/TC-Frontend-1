5810bf30038ccb73510f1f58228e957cb1f5b3cb
ankitlakra <ankitlakra157@gmail.com>
2023-09-08 16:59:12 +0530

    vig page

--- src/sections/hero/VigyaanHero.style.js
+++ src/sections/hero/VigyaanHero.style.js
@@ -74,7 +74,6 @@ const VigyaanStyleWrapper = styled.section`
     height: 200px ;
     animation: ${Circle} 15s linear infinite;
   }
-
   .vigyaan-planet {
     left: 50%;
     position: relative;
@@ -82,11 +81,11 @@ const VigyaanStyleWrapper = styled.section`
     transform: translateX(-50%);
     width: 70vw;
     z-index: 1;
-}
-.vigyaan-font p{
-  font-family: angora;
-}
-.vigyaan-font {
+  }
+  .vigyaan-font p{
+    font-family: angora;
+  }
+  .vigyaan-font {
   
   // -webkit-animation: cosmos-animation 3s;
   // animation: cosmos-animation 3s;
@@ -102,9 +101,9 @@ const VigyaanStyleWrapper = styled.section`
   z-index: 2;
   font-weight: 900;
   text-shadow: 2px 2px 5px #FE7BE5D1;
-}
+  }
 
-.vigyaan-desc{
+  .vigyaan-desc{
   position:absolute;
   top: 53vh;
   z-index:2;
@@ -112,7 +111,7 @@ const VigyaanStyleWrapper = styled.section`
   color:white;
   z-index: 2;
 }
-.vigyaan-ring {
+  .vigyaan-ring {
   -webkit-animation: ring-animation 3s;
   animation: ring-animation 3s;
   -webkit-animation-iteration-count: 1;
@@ -128,407 +127,139 @@ const VigyaanStyleWrapper = styled.section`
   z-index: 2;
 }
 
-.btncontainer{
+  .btncontainer{
   position: absolute;
   top: 55%;
   left: 50%;
   transform: translate(-50%, -50%);
-  padding: 10px;
   z-index: 3;
+ }
+
+ @media (max-width: 1121px) {
+  
+  .vigyaan-font {
+    font-size: 4rem; 
+  }
+
+  .vigyaan-desc {
+    padding: 0 5rem 0 5rem; 
+  }
+
+  .btncontainer{
+    top: 70%;
+  }
+  @media (max-width: 890px) {
+    height:100vh;
+  
+    .vigyaan-font {
+      font-size: 3rem; 
+      top: 35vh;
+    }
+  
+    .vigyaan-desc {
+      padding: 0 5rem 0 5rem;
+      font-size: 1rem;
+      top: 50vh;
+    }
+  
+    .btncontainer{
+      top: 70%;
+    }
+}
+@media (max-width: 800px) {
+  .planet1{
+    height: 50px ;
+  }
+  .planet2{
+     height: 100px ;
+  }
+  .planet3{
+    height: 100px ;
+  }
+}
+@media (max-width: 726px) {
+  .vigyaan-ring {
+    display:none;
+  }
+  .vigyaan-font {
+    font-size: 2.5rem; 
+    top: 25vh;
+  }
+
+  .vigyaan-desc {
+    padding: 0 5rem 0 5rem;
+    font-size: 1rem;
+    top: 40vh;
+  }
+  .vigyaan-planet{
+    margin-top: 50px;
+  }
+}
+@media (max-width: 515px) {
+  .vigyaan-font {
+    font-size: 2rem; 
+    top: 25vh;
+  }
+
+  .vigyaan-desc {
+    padding: 0 5rem 0 5rem;
+    font-size: 1rem;
+    top: 40vh;
+  }
+  .btncontainer{
+    top: 80%;
+  }
+  
+
+}
+@media (max-width: 426px) {
+  .vigyaan-font {
+    font-size: 1.8rem; 
+    top: 25vh;
+    left: 28%;
+  }
+  .vigyaan-desc {
+    padding: 0 1rem 0 1rem;
+    font-size: 1rem;
+    top: 35vh;
+  }
+  .planet1{
+    height: 40px ;
+  }
+  .planet2{
+     height: 70px ;
+  }
+  .planet3{
+    height: 70px ;
+  }
+}
+@media (max-width: 330px) {
+  .vigyaan-font {
+    font-size: 1.6rem; 
+    top: 24vh;
+    left: 26%;
+  }
+  .vigyaan-desc {
+    padding: 0 1rem 0 1rem;
+    font-size: 1rem;
+    top: 35vh;
+  }
+}
+@media (max-width: 280px) {
+  .btncontainer{
+    top: 90%;
+  }
+}
+@media (max-width: 251px) {
+  .vigyaan-font {
+    font-size: 1rem; 
+    top: 20vh;
+    left: 28%;
+  }
 }
 
-  @media only screen and (max-width: 1441px) {
-    .asto{
-      position: absolute ;
-      top: 110vh ;
-      left: 5vw;
-      height: 400px ;
-    }
-  
-    .planet1{
-      position: absolute;
-      top: 10vh;
-      right: 0%;
-      height: 100px ;
-      animation: ${rotate360} 15s linear infinite;
-    }
-    .planet2{
-       animation: ${quarterCircle} 4s linear infinite;
-       left: 0%;
-       height: 200px ;
-    }
-    .planet3{
-      position: absolute;
-      top: 30vh; 
-      left: 2vw;
-      height: 200px ;
-      animation: ${Circle} 15s linear infinite;
-    }
-  
-    .vigyaan-planet {
-      left: 50%;
-      position: relative;
-      -webkit-transform: translateX(-50%);
-      transform: translateX(-50%);
-      width: 70vw;
-      z-index: 1;
-  }
-  .vigyaan-font {
-    // -webkit-animation: cosmos-animation 3s;
-    // animation: cosmos-animation 3s;
-    left: 32%;
-    // opacity: .5;
-    position: absolute;
-    top: 35vh;
-    // -webkit-transform: translateX(-50%);
-    // transform: translateX(-50%);
-    // width: 45vw;
-    font-size: 5.5rem;
-    color:white;
-    z-index: 2;
-    font-weight: 900;
-    text-shadow: 2px 2px 5px #FE7BE5D1;
-  }
-  .vigyaan-desc{
-    position:absolute;
-    top: 53vh;
-    z-index:2;
-    padding: 0 13rem 0 10rem;
-    color:white;
-    z-index: 2;
-  }
-  .vigyaan-ring {
-    -webkit-animation: ring-animation 3s;
-    animation: ring-animation 3s;
-    -webkit-animation-iteration-count: 1;
-    animation-iteration-count: 1;
-    left: 50%;
-    position: absolute;
-    top: 40vw;
-    top: 58vh;
-    -webkit-transform: translateX(-50%);
-    transform: translateX(-50%);
-    width: 90vw;
-    width: 70vw;
-    z-index: 2;
-  }
-  
-  .btncontainer{
-    position: absolute;
-    top: 55%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    padding: 10px;
-    z-index: 3;
-  }
-  }
-  @media only screen and (max-width: 1025px) {
-    .asto{
-      position: absolute ;
-      top: 90vh ;
-      left: 5vw;
-      height: 300px ;
-    }
-  
-    .planet1{
-      position: absolute;
-      top: 10vh;
-      right: 0%;
-      height: 80px ;
-      animation: ${rotate360} 15s linear infinite;
-    }
-    .planet2{
-       animation: ${quarterCircle} 4s linear infinite;
-       left: 0%;
-       height: 180px ;
-    }
-    .planet3{
-      position: absolute;
-      top: 30vh; 
-      left: 2vw;
-      height: 180px ;
-      animation: ${Circle} 15s linear infinite;
-    }
-  
-    .vigyaan-planet {
-      left: 50%;
-      position: relative;
-      -webkit-transform: translateX(-50%);
-      transform: translateX(-50%);
-      width: 70vw;
-      z-index: 1;
-  }
-  .vigyaan-font {
-    // -webkit-animation: cosmos-animation 3s;
-    // animation: cosmos-animation 3s;
-    left: 26%;
-    // opacity: .5;
-    position: absolute;
-    top: 35vh;
-    // -webkit-transform: translateX(-50%);
-    // transform: translateX(-50%);
-    // width: 45vw;
-    font-size: 4.8rem;
-    color:white;
-    z-index: 2;
-    font-weight: 900;
-    text-shadow: 2px 2px 5px #FE7BE5D1;
-  }
-  .vigyaan-desc{
-    position:absolute;
-    top: 53vh;
-    z-index:2;
-    padding: 0 12rem 0 12rem;
-    color:white;
-    z-index: 2;
-  }
-  .vigyaan-ring {
-    -webkit-animation: ring-animation 3s;
-    animation: ring-animation 3s;
-    -webkit-animation-iteration-count: 1;
-    animation-iteration-count: 1;
-    left: 50%;
-    position: absolute;
-    top: 65vh;
-    -webkit-transform: translateX(-50%);
-    transform: translateX(-50%);
-    width: 70vw;
-    z-index: 2;
-  }
-  
-  .btncontainer{
-    position: absolute;
-    top: 70%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    padding: 10px;
-    z-index: 3;
-  }
-  }
+}
 
-  @media only screen and (max-width: 769px) {
-    height:100vh;
-    .vigyaan-font {
-      left: 32%;
-      position: absolute;
-      top: 22vh;
-      font-size: 2.8rem;
-      color:white;
-      z-index: 2;
-      font-weight: 900;
-      text-shadow: 2px 2px 5px #FE7BE5D1;
-    }
-    .vigyaan-desc{
-      position:absolute;
-      top: 35vh;
-      color:white;
-      z-index: 2;
-      padding: 0 10rem 0 10rem;
-    }
-    .btncontainer{
-      position: absolute;
-      top: 90%;
-      left: 50%;
-      transform: translate(-50%, -50%);
-      padding: 5px;
-      z-index: 3;
-      font-size: 18px;
-    }
-    .asto{
-      position: absolute ;
-      top: 70vh ;
-      left: 5vw;
-      height: 200px ;
-    }
-    .planet1{
-      position: absolute;
-      top: 10vh;
-      right: 0%;
-      height: 70px ;
-      animation: ${rotate360} 15s linear infinite;
-    }
-    .planet2{
-      animation: ${quarterCircle} 4s linear infinite;
-      left: 0%;
-      height: 160px ;
-   }
-   .planet3{
-     position: absolute;
-     top: 30vh; 
-     left: 2vw;
-     height: 160px ;
-     animation: ${Circle} 15s linear infinite;
-   }
-   .vigyaan-ring {
-     display: none;
-   }
-  
-  }
-
-  @media only screen and (max-width: 426px) {
-     height: 100vh;
-
-    .vigyaan-planet {
-      top: 8vh;
-      left: 50%;
-      position: relative;
-      -webkit-transform: translateX(-50%);
-      transform: translateX(-50%);
-      width: 70vw;
-      z-index: 1;
-  }
-    .vigyaan-font {
-      left: 28%;
-      position: absolute;
-      top: 28vh;
-      font-size: 1.8rem;
-      color:white;
-      z-index: 2;
-      font-weight: 900;
-      text-shadow: 2px 2px 5px #FE7BE5D1;
-    }
-    .vigyaan-desc{
-      position:absolute;
-      top: 40vh;
-      font-size:1rem;
-      color:white;
-      z-index: 2;
-      padding: 0 1rem 0 1rem;
-    }
-    .btncontainer{
-      position: absolute;
-      top: 80%;
-      left: 50%;
-      transform: translate(-50%, -50%);
-      padding: 5px;
-      z-index: 3;
-    }
-    .metaportal_fn_button_3{
-      font-size: 15px;
-    }
-    .asto{
-      display: none;
-      position: absolute ;
-      top: 70vh ;
-      left: 5vw;
-      height: 100px ;
-    }
-    .planet1{
-      position: absolute;
-      top: 10vh;
-      right: 0%;
-      height: 60px ;
-      animation: ${rotate360} 15s linear infinite;
-    }
-    .planet2{
-      animation: ${quarterCircle} 4s linear infinite;
-      left: 0%;
-      height: 80px ;
-   }
-   .planet3{
-     position: absolute;
-     top: 30vh; 
-     left: 2vw;
-     height: 80px ;
-     animation: ${Circle} 15s linear infinite;
-   }
-   .vigyaan-ring {
-     display: none;
-   }
-  }
-  @media only screen and (max-width: 376px) {
-    height: 100vh;
-
-    .vigyaan-planet {
-      top: 8vh;
-      left: 50%;
-      position: relative;
-      -webkit-transform: translateX(-50%);
-      transform: translateX(-50%);
-      width: 70vw;
-      z-index: 1;
-  }
-    .vigyaan-font {
-      left: 29%;
-      position: absolute;
-      top: 25vh;
-      font-size: 1.6rem;
-      color:white;
-      z-index: 2;
-      font-weight: 900;
-      text-shadow: 2px 2px 5px #FE7BE5D1;
-    }
-    .vigyaan-desc{
-      position:absolute;
-      top: 40vh;
-      color:white;
-      z-index: 2;
-      padding: 0 1rem 0 1rem;
-    }
-    .btncontainer{
-      position: absolute;
-      top: 85%;
-      left: 50%;
-      transform: translate(-50%, -50%);
-      padding: 5px;
-      z-index: 3;
-      width: 70%;
-    }
-    .metaportal_fn_button_3{
-      font-size: 14px;
-
-    }
-    .asto{
-      display: none;
-      position: absolute ;
-      top: 70vh ;
-      left: 5vw;
-      height: 100px ;
-    }
-    .planet1{
-      position: absolute;
-      top: 10vh;
-      right: 0%;
-      height: 50px ;
-      animation: ${rotate360} 15s linear infinite;
-    }
-    .planet2{
-      animation: ${quarterCircle} 4s linear infinite;
-      left: 0%;
-      height: 70px ;
-   }
-   .planet3{
-     position: absolute;
-     top: 30vh; 
-     left: 2vw;
-     height: 70px ;
-     animation: ${Circle} 15s linear infinite;
-   }
-   .vigyaan-ring {
-     display: none;
-   }
-   @media only screen and (max-width: 321px) {
-    .vigyaan-font {
-      left: 28%;
-      position: absolute;
-      top: 22vh;
-      font-size: 1.4rem;
-      color:white;
-      z-index: 2;
-      font-weight: 900;
-      text-shadow: 2px 2px 5px #FE7BE5D1;
-    }
-    .btncontainer{
-      position: absolute;
-      top: 85%;
-      left: 50%;
-      transform: translate(-50%, -50%);
-      padding: 5px;
-      z-index: 3;
-      width: 80%;
-    }
-   }
-  }
 `;
 
 export default VigyaanStyleWrapper;

25b4d8e6bcab93f270553fd720f5d8a35b453cf3
Aniket Kumar <kraniket123654@gmail.com>
2023-10-15 11:12:41 +0530

    added event cards 2

--- src/pages/Event1Page.js
+++ src/pages/Event1Page.js
@@ -1,9 +1,11 @@
 import React from "react";
 import EventForm from "../forms/EventForm";
+import Title from "../components/Title";
 
 const Event1Page = ()=>{
     return(
         <>
+        <Title color={"Event"} noncolor={"Name"} />
         <EventForm/>
         </>
     )

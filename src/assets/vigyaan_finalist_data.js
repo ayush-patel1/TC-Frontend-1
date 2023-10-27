c6acfd5f4c3eb3ce1cfbfbf9c363ac338373c79f
Aniket Kumar <kraniket123654@gmail.com>
2023-10-28 03:24:52 +0530

    added vigyaan winners card

--- src/assets/vigyaan_finalist_data.js
+++ src/assets/vigyaan_finalist_data.js
@@ -53,4 +53,49 @@ export const vigyaanFinalistList = [
             team5:"Team 5 Name",
             branch: "Branch"
         },
+        {
+            id:6,
+            team1:"Team 1 Name",
+            team2:"Team 2 Name",
+            team3:"Team 3 Name",
+            team4:"Team 4 Name",
+            team5:"Team 5 Name",
+            branch: "Branch"
+        },
+        {
+            id:7,
+            team1:"Team 1 Name",
+            team2:"Team 2 Name",
+            team3:"Team 3 Name",
+            team4:"Team 4 Name",
+            team5:"Team 5 Name",
+            branch: "Branch"
+        },
+        {
+            id:8,
+            team1:"Team 1 Name",
+            team2:"Team 2 Name",
+            team3:"Team 3 Name",
+            team4:"Team 4 Name",
+            team5:"Team 5 Name",
+            branch: "Branch"
+        },
+        {
+            id:9,
+            team1:"Team 1 Name",
+            team2:"Team 2 Name",
+            team3:"Team 3 Name",
+            team4:"Team 4 Name",
+            team5:"Team 5 Name",
+            branch: "Branch"
+        },
+        {
+            id:10,
+            team1:"Team 1 Name",
+            team2:"Team 2 Name",
+            team3:"Team 3 Name",
+            team4:"Team 4 Name",
+            team5:"Team 5 Name",
+            branch: "Branch"
+        },
   ];
--- src/components/VigyaanCard.js
+++ src/components/VigyaanCard.js
@@ -6,16 +6,16 @@ const VigyaanCard = (props) => {
   return (
     <div className='vigyaan--card'>
         <div>
+            <h4>
+                {props.branch}
+            </h4>
+        </div>
+        <div>
         <h3>{props.team1}</h3>
         <h3>{props.team2}</h3>
         <h3>{props.team3}</h3>
         <h3>{props.team4}</h3>
         <h3>{props.team5}</h3>
-        </div>
-        <div>
-            <h4>
-                {props.branch}
-            </h4>
         </div>
     </div>
   )

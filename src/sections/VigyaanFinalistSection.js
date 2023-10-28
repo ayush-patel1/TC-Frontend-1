650d38368cbd85b59cf7aba9209e94222558a2a0
Deewakar <deewakarrao59@gmail.com>
2023-10-28 12:00:53 +0530

    changes updated

---
+++ src/sections/VigyaanFinalistSection.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import Title from '../components/Title'
+import VigyaanCard from '../components/VigyaanCard'
+import "./VigyaanFinalistSection.css"
+import {vigyaanFinalistList} from '../assets/vigyaan_finalist_data';
+
+const VigyaanFinalistSection = () => {
+  return (
+    <div className='vigyaan-finalist-section'>
+        <Title color="VIGYAAN" noncolor="FINALISTS"/>
+        <div className='vigyaan-card--container'>
+        {vigyaanFinalistList.map(item=>{
+          return(
+            <VigyaanCard
+            team1={item.team1}
+            team2={item.team2}
+            team3={item.team3}
+            team4={item.team4}
+            team5={item.team5}
+            team6={item.team6}
+            team7={item.team7}
+            team8={item.team8}
+            branch={item.branch}
+            />
+          )
+        })}
+        </div>
+    </div>
+  )
+}
+
+export default VigyaanFinalistSection

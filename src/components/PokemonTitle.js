369497186e73e6d5ecdd7a7a81559ef1e0609f4c
Nehil Sahu <nehilsahu281@gmail.com>
2024-12-11 10:38:57 +0530

    changes (#239)
    * changes in rules
    
    * chnages
    
    * test
    
    * changes
    
    * changes
    
    * changes
    
    * updated pokemontitle.css

--- src/components/PokemonTitle.js
+++ src/components/PokemonTitle.js
@@ -1,16 +1,14 @@
-import React from 'react'
-import "./PokemonTitle.module.css"
-// import { Line } from './Line'
+import React from 'react';
+import styles from './PokemonTitle.module.css';
 
 const PTitle = (props) => {
   return (
-    <div className="ptitle" >
-        <h1 className="pheading">
-           <span>{props.color}</span> {props.noncolor}
-        </h1>
-        {/* <Line/> */}
-      </div>
-  )
-}
+    <div className={styles.ptitle}>
+      <h1 className={styles.pheading}>
+        <span>{props.color}</span> {props.noncolor}
+      </h1>
+    </div>
+  );
+};
 
-export default PTitle
+export default PTitle;

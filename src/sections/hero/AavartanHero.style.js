49f0e7ac1998ffe00dc41fe1d6d9b7dfb0fdd814
Nehil Sahu <nehilsahu281@gmail.com>
2025-01-07 00:49:33 +0530

    updated aavartan (#258)
    * changes in rules
    
    * chnages
    
    * test
    
    * changes
    
    * changes
    
    * changes
    
    * updated pokemontitle.css
    
    * changes in vigyaan
    
    * updated sponsors page
    
    * updated sponsors page
    
    * updated aavartan
    
    * changed aavartan
    
    * updated aavartan
    
    * updated timeline
    
    * updated aavartan
    
    * updated aavartan

--- src/sections/hero/AavartanHero.style.js
+++ src/sections/hero/AavartanHero.style.js
@@ -56,20 +56,20 @@ const AavartanStyleWrapper = styled.section`
   .container{
     z-index:100;
     height:100vh !important;
-    widht:100vw;
+    width:100vw;
     margin:0;
     position:relative;
     top:-20vh;
   }
    
-  .ufo_shape {
+  /* .ufo_shape {
     position: absolute;
     top: 2%;
     left: 10%;
     z-index: 0;
-  }
+  } */
   .moving-cloud-container {
-    position: absolute; /* Ensure it's positioned explicitly */
+    position: absolute;
   top: -18vh;
   left: 0;
   width: 100%;
@@ -80,7 +80,7 @@ const AavartanStyleWrapper = styled.section`
 
 .moving-cloud {
   position: absolute;
-  animation: moveCloud 200s linear infinite;
+  animation: moveCloud 300s linear infinite ;
   repeat:infinite;
   z-index: 400;
 }
@@ -91,7 +91,10 @@ const AavartanStyleWrapper = styled.section`
     transform: translateX(-80%); 
   }
   50% {
-    transform: translateX(5vw); 
+    transform: translateX(1vw); 
+  }
+  100%{
+    transform: translateX(-80%); 
   }
 }
 .plants{
@@ -129,7 +132,7 @@ const AavartanStyleWrapper = styled.section`
          width:100%;
       }
 
-      .rocket_thumb {
+      /* .rocket_thumb {
         position: absolute;
         bottom: 0.5rem;
         right: 2rem;
@@ -145,302 +148,171 @@ const AavartanStyleWrapper = styled.section`
         animation: ${rocketAnimation2} 5s infinite;
         z-index: 0;
         height: 25vw;
-      }
+      } */
   
-.decrp {
-    color: white; /* White text for contrast */
-    font-weight: 300;
-    font-size:3vh;
-    line-height: 35px;
-    margin-top: 60px;
-    width:80vw;
-    user-select: none;
-    text-align: center;
-    text-shadow: 1px 0px 3px black;
-    font-family: "AavartanFont";
-    position: absolute;
-    top: 47vh;
-    left: 10%; /* Syncs with background's left */
-    right: 10%; /* Syncs with background's right */
-    z-index: 2;
-    border-radius:20px;
-    background: rgba(96, 31, 19, 0.6); /* Slightly transparent black with more opacity */
-    backdrop-filter: blur(5px); /* Increased blur effect */
-    padding:; /* Space around the content */
-    border-radius: 5px; /* Smooth rounded corners */
-    // box-shadow: 0 0 10px 5px rgba(0, 0, 0, 0.5); /* Slightly visible shadow */
-}
-
-  
-    
-  }
-
-  @media only screen and (max-width: 1441px) {
-    .gamfi_v2_hero_left h2 {
-      font-size: 8rem; /* Adjust font size for 1440px */
-    }
-
-    .gamfi_v2_hero_right {
-      .gamfi_v2_hero_thumb {
-        .rocket_thumb {
-          height: 400px;
-        }
-        .rocket_thumb2 {
-          height: 280px;
-        }
-      }
-    }
-   .btncontainer{
-       top:85% !important;
-       left:50% !important;
-       translate:transform(-50%,-50%);
-    }
-    .decrp {
-      font-size:max(1.2vh,1.5rem) !important;
-      line-height:3rem !important;
-      padding:0 5px !important;
-      top:58% !important;
-      left:10% !important;
-      width:95vw;
-    }
-    .aavartan-font{
-      top:5% !important;
-      left:20% !important;
-      width:60vw !important;
-      height:20vh !important;
-    }
-  }
-
-  @media only screen and (max-width: 1025px) {
-    .btncontainer{
-      top:90% !important;
-      left:10%;
-      transform:translate(-50%,-50%);
-    }
-
-    .aavartan-font {
-     top:10% !important;
-    height: 18vh !important;
-    width: 60vw !important;
-    left: 18% !important;
-    }
-  .gamfi_v2_hero_left .gamfi_v2_hero_thumb .rocket_thumb2 {
-  position: absolute;
-  top: 30%;
-  left: 1rem;
-  animation: jxHqgd 5s infinite;
-  z-index: 0;
-  height: 25vw;
-   } 
-.decrp {
-  color: black;
+  .decrp {
+  color: white;
   font-weight: 300;
-  font-size:1vh rem !important;
-  line-height: 3rem !important;
-  margin-top: 10px;
+  font-size: 3vh;
+  line-height: 35px;
+  margin-top: 60px;
+  width: 80vw;
+  user-select: none;
   text-align: center;
-  text-shadow: 1px 1px 5pxrgb(73, 6, 6);
+  text-shadow: 1px 0px 3px black;
   font-family: "AavartanFont";
   position: absolute;
-  background: rgba(149, 8, 8, 0.6);
-  top: 60% !important;
-  left:3.5vw !important;
-  width: 90vw !important;
+  top: 47vh;
+  left: 10%;
+  right: 10%;
   z-index: 2;
-  }
-     }
-
-  @media only screen and (max-width: 861px) {
-    .btncontainer {
-      top:90% !important;
-      left:10%;
-    }
-
-    .aavartan-font {
-    top: 10% !important;
-    height: 19vh !important;
-    width: 65vw !important;
-    left: 15% !important;
-    }
-  .gamfi_v2_hero_left .gamfi_v2_hero_thumb .rocket_thumb2 {
-  position: absolute;
-  top: 30%;
-  left: 1rem;
-  animation: jxHqgd 5s infinite;
-  z-index: 0;
-  height: 25vw;
-   } 
-.decrp {
-  font-weight: 300;
-  line-height: 3rem !important;
-  margin-top: 10px;
-  text-align: center;
-  position: absolute;
-  top: 60% !important;
-  left:4% !important;
-  width: 90vw !important;
-  z-index: 2;
-  }
+  border-radius: 20px;
+  background: rgba(96, 42, 33, 0.6);
+  backdrop-filter: blur(10px);
+  padding: 1rem;
+  height: auto; /* Let it expand automatically */
+  max-height: 90vh; /* Restrict to fit within the screen */
+  overflow-y: auto; /* Enable scrolling for overflow */
+  box-shadow: 0 0 10px 5px rgba(0, 0, 0, 0.5);
 }
 
-  @media only screen and (max-width: 769px) {
-
-    .btncontainer {
-      top:90% !important;
-      left:10%;
-    }
-
-    .aavartan-font {
-    top: 10% !important;
-    height: 19vh !important;
-    width: 68vw !important;
-    left: 18% !important;
-    }
-  .gamfi_v2_hero_left .gamfi_v2_hero_thumb .rocket_thumb2 {
-  position: absolute;
-  top: 30%;
-  left: 1rem;
-  animation: jxHqgd 5s infinite;
-  z-index: 0;
-  height: 25vw;
-   } 
-.decrp {
-    position:absolute;
-    top:58% !important;
-    left:4% !important;
+@media only screen and (min-width: 1921px) and (max-width: 2560px) {
+  .decrp {
+    font-size: 5rem;
+    line-height: 2.5rem;
+    padding: 2.5rem;
+    width: 100%;
+    position: absolute;
+    top: 57%;
+    left: 71%;
+    transform: translate(-50%, -50%);
+    overflow-y: auto;
+    line-height: 6rem;
+    padding:2rem;
   }
-  }
-
-  @media only screen and (max-width: 571px) {
-    .btncontainer{
-       top:90% !important;
-       left:43% !important;
-    }
-    .decrp {
-      // font-size:1.4rem !important;
-      line-height:2.2rem !important;
-      padding:0 5px !important;
-      top:60% !important;
-      left:5% !important;
-      width:100vw;
-    }
-    .aavartan-font{
-      // top:10% !important;
-      left:12% !important;
-      width:80vw !important;
-      height:20vh !important;
-    }
-    .gamfi_v2_hero_left .rocket_thumb {
-      height: 80px !important;
-    }
-
-    .rocket_thumb2 {
-      height: 80px;
-    }
-    
-    .colmd2{
-      width:100vw;
-    }
-  }
-
-  @media (max-width: 426px) {
-
-    .btncontainer{
-       top:90% !important;
-       left:43% !important;
-    }
-    .decrp {
-      line-height:2rem !important;
-      padding-bottom:2px !important;
-      padding:0 5px !important;
-      bottom:13% !important;
-      left:5% !important;
-      width:100vw;
-    }
-
-    .aavartan-font{
-      // top:3% !important;
-      left:12% !important;
-      width:80vw !important;
-      height:20vh !important;
-    }
-    .gamfi_v2_hero_left .rocket_thumb {
-      height: 80px !important;
-    }
-
-    .rocket_thumb2 {
-      height: 80px;
-    }
-    
-    .colmd2{
-      width:100vw;
-    }
-  }
-
-  @media only screen and (max-width: 376px) {
-
   .aavartan-font {
-       height: 15vh;
-    //  top:0.8% !important;
-     width: 70vw;
-}
-
-      .decrp{
-        bottom:18% !important;
-        font-size:1rem !important;
-        line-height:1.5rem !important;
-        margin-left:-2px !important;
-        padding:10px 5px !important;
-         text-align:center;
-      }
-         .btncontainer{
-            top:90% !important;
-            left:43% !important;
-         }
-}
-    
-
-
-  @media only screen and (max-width: 321px) {
-    .gamfi_v2_hero_left h2 {
-      font-size: 2rem;
+      width:70vw;
+      position: absolute;
+      top: 8%;
+      left:23%;
     }
+  
+}
+@media only screen and (min-width: 600px) and (max-width: 1024px) {
+  .decrp {
+    font-size: 1.4rem;
+    line-height: 1.8rem;
+    width: 85%;
+    top: 40vh; 
+    height: auto;
+    max-height: 50vh; 
+    overflow-y: auto;
+    padding: 1.5rem;
+  }
+}
 
-    .metaportal_fn_button_3 {
-      bottom: 16vh;
-      left: 10%;
+  @media only screen and (max-width: 890px) {
+    .aavartan-font {
+      width:40vw;
     }
   }
 
-  @media only screen and (max-width: 301px) {
-    .btncontainer {
-      top: 90%;
-      width: 90%;
+  @media only screen and (max-width: 704px) {
+    .aavartan-font {
+      width:45vw;
     }
-
-    .gamfi_v2_hero_thumb .rocket_thumb {
-      height: 100px;
-    }
-
-    .rocket_thumb2 {
-      height: 80px;
-      bottom: 5rem;
+    .decrp{
+      padding: 1rem;
+      font-size: 1.2rem;
     }
   }
+  @media only screen and (max-width: 592px){
+    .decrp{
+      width:85vw;
+      position: absolute;
+      left: 7.5%;
+    }
+     .aavartan-font {
+      position: absolute;
+      left: 28%;
+
+    }
+  }
+
+  @media only screen and (max-width: 554px){
+    .decrp{
+      font-size:1.17rem;
+    }
+    .aavartan-font {
+      width: 60vw;
+      position: absolute;
+      left: 22.5%;
+    }
+  }
+  @media only screen and (max-width: 470px) {
+  .decrp {
+    font-size: 0.9rem;
+    line-height: 1.3rem;
+    padding: 1rem;
+    height: auto;
+    overflow-y: auto;
+  }
+  .aavartan-font {
+      width: 90vw;
+      position: absolute;
+      left: 6.5%;
+    }
+}
+
+@media only screen and (max-width: 399px) and (min-width: 350px) {
+  .decrp {
+    font-size: 0.95rem;
+    padding: 0.8rem;
+    line-height: 1.3rem;
+    height: auto;
+  }
+}
+@media only screen and (max-width: 360px) {
+  .decrp {
+    font-size: 0.9rem;
+    padding: 0.5rem;
+    line-height: 1.2rem;
+    height: auto;
+    max-height: 80vh;
+    overflow-y: auto;
+  }
+}
+
+@media only screen and (max-width: 349px) and (min-width: 300px) {
+  .decrp {
+    font-size: 0.85rem;
+    padding: 0.6rem;
+    line-height: 1.2rem;
+    height: auto;
+  }
+}
+
+@media only screen and (max-width: 300px) and (min-width: 262px) {
+  .decrp {
+    font-size: 0.8rem;
+    padding: 0.5rem;
+    line-height: 1.2rem;
+    height: auto;
+    overflow-y: auto;
+  }
+}
+
 
   @media only screen and (max-width: 261px) {
-    .btncontainer {
-      top: 95%;
-    }
-
-    .gamfi_v2_hero_left h2 {
-      font-size: 1rem;
-    }
-
-    .decrp{
-      font-size: 0.8rem;
-    }
+  .decrp {
+    height: auto; 
+    overflow-y: auto; 
+    font-size: 0.8rem; 
+    line-height: 1.2rem;
+    padding: 0.5rem; 
   }
+}
+
 `;
 
 export default AavartanStyleWrapper;

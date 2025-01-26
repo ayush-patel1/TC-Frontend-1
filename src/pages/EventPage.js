ffbf7f7ec0f53c5c1544761a3d287bd9da784802
Nehil <nehilsahu281@gmail.com>
2025-01-26 23:56:11 +0530

    changes in event

--- src/pages/EventPage.js
+++ src/pages/EventPage.js
@@ -1,9 +1,11 @@
 import React from "react";
 import EventSec from "../sections/EventSec";
+import FloatingImage from '../sections/FloatingImage';
 
 const EventPage = ()=>{
     return(
         <EventSec/>
+        
     )
 }
 

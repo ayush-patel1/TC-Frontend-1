a9fb28e5c904acc9fe72dc962e4a97b333887a69
Nehil <nehilsahu281@gmail.com>
2025-01-26 17:23:15 +0530

    events updated

--- src/assets/Events_data.js
+++ src/assets/Events_data.js
@@ -1,203 +1,203 @@
-import event1 from '../assets/eventsAssets/Images/bgmi.webp'
-import event2 from '../assets/eventsAssets/Images/roboSoccer.webp'
-import event3 from '../assets/eventsAssets/Images/bgmi.webp'
-import event4 from '../assets/eventsAssets/Images/bgmi.webp'
-import event5 from '../assets/eventsAssets/Images/bgmi.webp'
-import event6 from '../assets/eventsAssets/Images/speedCubing.webp'
-import event7 from '../assets/eventsAssets/Images/bgmi.webp'
-import event8 from '../assets/eventsAssets/Images/bgmi.webp'
-import event9 from '../assets/eventsAssets/Images/valorant.webp'
-import event10 from '../assets/eventsAssets/Images/bgmi.webp'
-import event11 from '../assets/eventsAssets/Images/bgmi.webp'
-import event12 from '../assets/eventsAssets/Images/bgmi.webp'
-import event13 from '../assets/eventsAssets/Images/treasureHunt.webp'
-import event14 from '../assets/eventsAssets/Images/bgmi.webp'
+import event1 from '../assets/eventsPoster/technoGraphix.webp'
+import event2 from '../assets/eventsPoster/comingSoon.webp'
+import event3 from '../assets/eventsPoster/comingSoon.webp'
+import event4 from '../assets/eventsPoster/comingSoon.webp'
+import event5 from '../assets/eventsPoster/comingSoon.webp'
+import event6 from '../assets/eventsPoster/comingSoon.webp'
+import event7 from '../assets/eventsPoster/ecopolis.webp'
+import event8 from '../assets/eventsPoster/comingSoon.webp'
+import event9 from '../assets/eventsPoster/pixelPlayground.webp'
+import event10 from '../assets/eventsPoster/comingSoon.webp'
+import event11 from '../assets/eventsPoster/comingSoon.webp'
+import event12 from '../assets/eventsPoster/comingSoon.webp'
+import event13 from '../assets/eventsPoster/comingSoon.webp'
+import event14 from '../assets/eventsPoster/comingSoon.webp'
 import event15 from '../assets/eventsAssets/Images/talentShow.webp'
-import event16 from '../assets/eventsAssets/Images/ticTacToe.jpg'
-import event17 from '../assets/eventsAssets/Images/bgmi.webp'
-import event18 from '../assets/eventsAssets/Images/bgmi.webp'
-import event19 from '../assets/eventsAssets/Images/bgmi.webp'
-import event20 from '../assets/eventsAssets/Images/bgmi.webp'
-import event21 from '../assets/eventsAssets/Images/bgmi.webp'
-import event22 from '../assets/eventsAssets/Images/bgmi.webp'
+import event16 from '../assets/eventsPoster/comingSoon.webp'
+import event17 from '../assets/eventsPoster/comingSoon.webp'
+import event18 from '../assets/eventsPoster/openMic.webp'
+import event19 from '../assets/eventsPoster/speedCubing.jpg'
+import event20 from '../assets/eventsPoster/comingSoon.webp'
+import event21 from '../assets/eventsPoster/comingSoon.webp'
+import event22 from '../assets/eventsPoster/comingSoon.webp'
 
 
 export const EventsList = [
-  //   {
-  //     id: 1,
-  //     name: "TECHNO GRAPHIX",
-  //     imgUrl: event1,
-  //     date:"comming soon",
-  //     time:"comming soon",
-  //     venue:"G-4",
-  //     des: "An Event based on Graphic designing/poster making based on the theme of the fest.",
-  //     link:"/events/technoGraphixForm",
-  //     deadline:"comming soon"
-  //   },
-  //   {
-  //     id: 2,
-  //     name: "DSA MARATHON",
-  //     imgUrl: event2,
-  //     date:"comming soon",
-  //     time:"comming soon",
-  //     venue:"S-4",
-  //     des: "An event based on quick selection and solving maximum questions from a set of 30 Standard DSA questions.",
-  //     link:"/events/dsaMarathonForm",
-  //     deadline:"comming soon"
-  //   },
-  //   {
-  //     id: 3,
-  //     name: "DRONE RACING",
-  //     imgUrl: event3,
-  //     date:"comming soon",
-  //     time:"comming soon",
-  //     venue:"Outdoors",
-  //     des: "Each Teams have to bring their own drone and complete the given track in minimum time.",
-  //     link:"/events/droneRacingForm",
-  //     deadline:"comming soon"
-  //   },
-  //   {
-  //     id: 4,
-  //     name: "TECH CLASH",
-  //     imgUrl: event4,
-  //     date:"comming soon",
-  //     time:"comming soon",
-  //     venue:"Any Classroom",
-  //     des: "Tech Clash is a debate competition on cutting-edge tech topics like AI, Blockchain, and data privacy.",
-  //     link:"/events/techClashForm",
-  //     deadline:"comming soon"
-  //   },
-  //   {
-  //     id: 5,
-  //     name: "CODE SPRINT RELAY",
-  //     imgUrl: event5,
-  //     date:"comming soon",
-  //     time:"comming soon",
-  //     venue:"CCC Lab",
-  //     des: "A dynamic, team-based coding challenge where teamwork, creativity, and problem-solving are tested.",
-  //     link:"/events/codeSprintRelayForm",
-  //     deadline:"comming soon"
-  //   },
-  //   {
-  //     id: 6,
-  //     name: "DIMENSIONS (AUTOCAD)",
-  //     imgUrl: event6,
-  //     date:"comming soon",
-  //     time:"comming soon",
-  //     venue:"Any Classroom",
-  //     des: "AUTOCAD enthusiasts can showcase their talent by creating designs.",
-  //     link:"/events/dimensionsForm",
-  //     deadline:"comming soon"
-  //   },
-  //   {
-  //     id: 7,
-  //     name: "ECOPOLIS",
-  //     imgUrl: event7,
-  //     date:"comming soon",
-  //     time:"comming soon",
-  //     venue:"G-4",
-  //     des: "Design with Creativity tackles real-life architectural challenges in India, We'll focus on the real problems of our own country.",
-  //     link:"/events/ecopolisForm",
-  //     deadline:"comming soon"
-  //   },
-  //   {
-  //     id: 8,
-  //     name: "ROBO WARS",
-  //     imgUrl: event8,
-  //     date:"comming soon",
-  //     time:"comming soon",
-  //     venue:"Outdoors",
-  //     des: "A fun and exhilarating event, where participants will bring their own small robots for an epic death match.",
-  //     link:"/events/roboWarsForm",
-  //     deadline:"comming soon"
-  //   },
-  //   {
-  //     id: 9,
-  //     name: "PIXEL PLAYGROUND",
-  //     imgUrl: event9,
-  //     date:"comming soon",
-  //     time:"comming soon",
-  //     venue:"comming soon",
-  //     des: "A fast-paced video editing competition where participants capture and craft a short film showcasing the 2-day fest.",
-  //     link:"/events/pixelPlaygroundForm",
-  //     deadline:"comming soon"
-  //   },
-  //   {
-  //     id: 10,
-  //     name: "OCTOBER SKY",
-  //     imgUrl: event10,
-  //     date:"comming soon",
-  //     time:"comming soon",
-  //     venue:"Outdoors",
-  //     des: "Self-made rocket launching event.",
-  //     link:"/events/octoberSkyForm",
-  //     deadline:"comming soon"
-  //   },
-  //   {
-  //     id: 11,
-  //     name: "ESCAPE THE MATRIX",
-  //     imgUrl: event11,
-  //     date:"comming soon",
-  //     time:"comming soon",
-  //     venue:"Lecture Hall",
-  //     des: "A survival quiz testing knowledge, problem-solving, and quick thinking to escape the virtual matrix.",
-  //     link:"/events/escapeTheMatrixForm",
-  //     deadline:"comming soon"
-  //   },
-  //   {
-  //     id: 12,
-  //     name: "Workshops",
-  //     imgUrl: event12,
-  //     date:"comming soon",
-  //     time:"comming soon",
-  //     venue:" E-Hall/Architecture Auditorium",
-  //     des: "Organize a paid 2-day workshop on Python, AI/ML, or Ethical Hacking in collaboration with renowned organizations like GFG.",
-  //     link:"/events/workshopsForm",
-  //     deadline:"comming soon"
-  //   },
-  //   {
-  //     id: 13,
-  //     name: "REVERSE CODING",
-  //     imgUrl: event13,
-  //     date:"comming soon",
-  //     time:"comming soon",
-  //     venue:"CCC",
-  //     des: "Reverse Coding is a competition where participants work backward from the output, testing creativity, logic, and problem-solving skills.",
-  //     link:"/events/reverseCodingForm",
-  //     deadline:"comming soon"
-  //   },
-  //   {
-  //     id: 14,
-  //     name: "CASE OPS- 3.0",
-  //     imgUrl: event14,
-  //     date:"comming soon",
-  //     time:"comming soon",
-  //     venue:"comming soon",
-  //     des: "A two-round competition for undergraduate students focusing on aptitude and case study problem-solving.",
-  //     link:"/events/caseOpsForm",
-  //     deadline:"comming soon"
-  //   },
-  //   {
-  //     id: 15,
-  //     name: "D-BUG SHOWDOWN",
-  //     imgUrl: event15,
-  //     date:"comming soon",
-  //     time:"comming soon",
-  //     venue:"comming soon",
-  //     des: "A two-round coding competition focusing on programming, debugging, and problem-solving skills.",
-  //     link:"/events/dBugShowdownForm",
-  //     deadline:"comming soon"
-  //   },
+    {
+      id: 1,
+      name: "TECHNO GRAPHIX",
+      imgUrl: event1,
+      date:"02/02/2025",
+      time:"4:00 PM - 5:00 PM",
+      venue:"G-4",
+      des: "An Event based on Graphic designing/poster making based on the theme of the fest.",
+      link:"/events/technoGraphixForm",
+      deadline:"31/01/2025"
+    },
+    {
+      id: 2,
+      name: "DSA MARATHON",
+      imgUrl: event2,
+      date:"01/02/2025",
+      time:"11:30 AM - 1:00 PM",
+      venue:"S-4",
+      des: "An event based on quick selection and solving maximum questions from a set of 30 Standard DSA questions.",
+      link:"/events/dsaMarathonForm",
+      deadline:"31/01/2025"
+    },
+    {
+      id: 3,
+      name: "DRONE RACING",
+      imgUrl: event3,
+      date:"02/02/2025",
+      time:"2:30 PM -4:00 PM",
+      venue:"Outdoors",
+      des: "Each Teams have to bring their own drone and complete the given track in minimum time.",
+      link:"/events/droneRacingForm",
+      deadline:"31/01/2025"
+    },
+    {
+      id: 4,
+      name: " ",
+      imgUrl: event4,
+      date:"comming soon",
+      time:"comming soon",
+      venue:"Any Classroom",
+      des: "Tech Clash is a debate competition on cutting-edge tech topics like AI, Blockchain, and data privacy.",
+      link:"/events/techClashForm",
+      deadline:"31/01/2025"
+    },
+    {
+      id: 5,
+      name: "CODE SPRINT RELAY",
+      imgUrl: event5,
+      date:"02/02/2025",
+      time:"11:30 AM - 1:00 PM",
+      venue:"CCC Lab",
+      des: "A team-based coding challenge testing teamwork, creativity, and problem-solving.",
+      link:"/events/codeSprintRelayForm",
+      deadline:"31/01/2025"
+    },
+    {
+      id: 6,
+      name: "DIMENSIONS (AUTOCAD)",
+      imgUrl: event6,
+      date:"01/02/2025",
+      time:"11:30 AM - 1:00 PM",
+      venue:"Any Classroom",
+      des: "AUTOCAD enthusiasts can showcase their talent by creating designs.",
+      link:"/events/dimensionsForm",
+      deadline:"31/01/2025"
+    },
+    {
+      id: 7,
+      name: "ECOPOLIS",
+      imgUrl: event7,
+      date:"02/02/2025",
+      time:"2:30 PM - 4:00 PM",
+      venue:"G-4",
+      des: "Design with Creativity tackles real-life architectural challenges in India, We'll focus on the real problems of our own country.",
+      link:"/events/ecopolisForm",
+      deadline:"31/01/2025"
+    },
+    {
+      id: 8,
+      name: "ROBO WARS",
+      imgUrl: event8,
+      date:"01/02/2025",
+      time:"11:30 AM- 1:00 PM",
+      venue:"Outdoors",
+      des: "A fun and exhilarating event, where participants will bring their own small robots for an epic death match.",
+      link:"/events/roboWarsForm",
+      deadline:"31/01/2025"
+    },
+    {
+      id: 9,
+      name: "PIXEL PLAYGROUND",
+      imgUrl: event9,
+      date:"01/02/2025",
+      time:"All day",
+      venue:"Online",
+      des: "A fast-paced video editing competition where participants create a short film of the 2-day fest.",
+      link:"/events/pixelPlaygroundForm",
+      deadline:"31/01/2025"
+    },
+    {
+      id: 10,
+      name: "OCTOBER SKY",
+      imgUrl: event10,
+      date:"comming soon",
+      time:"comming soon",
+      venue:"Outdoors",
+      des: "Self-made rocket launching event.",
+      link:"/events/octoberSkyForm",
+      deadline:"31/01/2025"
+    },
+    {
+      id: 11,
+      name: "ESCAPE THE MATRIX",
+      imgUrl: event11,
+      date:"02/02/2025",
+      time:"11:00 AM - 1:00 PM + 2:30 PM -3:30 PM",
+      venue:"Lecture Hall",
+      des: "A survival quiz testing knowledge, problem-solving, and quick thinking to escape the virtual matrix.",
+      link:"/events/escapeTheMatrixForm",
+      deadline:"31/01/2025"
+    },
+    {
+      id: 12,
+      name: "Workshops",
+      imgUrl: event12,
+      date:"02/02/2025",
+      time:"11:00 AM -1:00 PM +2:00 PM - 3:30 PM",
+      venue:" E-Hall/Architecture Auditorium",
+      des: "Organize a paid 2-day workshop on Python, AI/ML, or Ethical Hacking in collaboration with renowned organizations like GFG.",
+      link:"/events/workshopsForm",
+      deadline:"31/01/2025"
+    },
+    {
+      id: 13,
+      name: "REVERSE CODING",
+      imgUrl: event13,
+      date:"02/02/2025",
+      time:"3:00 PM - 5:00 PM",
+      venue:"CCC",
+      des: "Reverse Coding is a competition where participants work backward from the output, testing creativity, logic, and problem-solving skills.",
+      link:"/events/reverseCodingForm",
+      deadline:"31/01/2025"
+    },
+    {
+      id: 14,
+      name: "CASE OPS- 3.0",
+      imgUrl: event14,
+      date:"comming soon",
+      time:"comming soon",
+      venue:"comming soon",
+      des: "A two-round competition for undergraduate students focusing on aptitude and case study problem-solving.",
+      link:"/events/caseOpsForm",
+      deadline:"31/01/2025"
+    },
+    {
+      id: 15,
+      name: "D-BUG SHOWDOWN",
+      imgUrl: event15,
+      date:"comming soon",
+      time:"comming soon",
+      venue:"comming soon",
+      des: "A two-round coding competition focusing on programming, debugging, and problem-solving skills.",
+      link:"/events/dBugShowdownForm",
+      deadline:"31/01/2025"
+    },
     {
       id: 16,
       name: "MOCK CID",
       imgUrl: event16,
-      date:"comming soon",
-      time:"comming soon",
+      date:"01/02/2025",
+      time:"10:30 AM- 3:30 PM",
       venue:"Amul Parlor",
       des: "A thriller version of Treasure Hunt.",
       link:"/events/treasureHuntForm",
-      deadline:"coming soon"
+      deadline:"31/01/2025"
     },
     {
     id: 17,
@@ -208,40 +208,40 @@ export const EventsList = [
     venue:"Left Garden/Amul Parlor",
     des: "A classic kerplunk game of carefully removing the sticks from a tube without causing the marbles to fall.",
     link:"/events/giantKerplunkForm",
-    deadline:"coming soon"
+    deadline:"31/01/2025"
   },
   {
     id: 18,
     name: "OPEN MIC",
     imgUrl: event18,
-    date:"comming soon",
-    time:"comming soon",
+    date:"01/02/2025",
+    time:"2:00 PM - 5:00 PM",
     venue:"AMUL PARLOUR",
-    des: "A fun event on Day 1 where participants can entertain the audience with singing, poetry, mimicry, or any act. Groups can also perform as a band.",
+    des: "An event where participants showcase singing, poetry, mimicry, or any act, with group performances welcome.",
     link:"/events/openMicForm",
-    deadline:"comming soon"
+    deadline:"31/01/2025"
   },
   {
     id: 19,
     name: "SPEED CUBING",
     imgUrl: event19,
-    date:"comming soon",
-    time:"comming soon",
+    date:"01/02/2025",
+    time:"11:00 AM",
     venue:"Left Garden",
     des: "Cube solving competition.",
     link:"/events/speedCubingForm",
-    deadline:"comming soon"
+    deadline:"31/01/2025"
   },
   {
     id: 20,
     name: "VALORANT TOURNAMENT",
     imgUrl: event20,
-    date:"comming soon",
-    time:"comming soon",
+    date:"01/02/2025",
+    time:"2:30 PM onwards",
     venue:"CCC",
     des: "Valorant Gaming competition.",
     link:"/events/valorantForm",
-    deadline:"comming soon"
+    deadline:"31/01/2025"
   },
   {
     id: 21,
@@ -249,10 +249,10 @@ export const EventsList = [
     imgUrl: event21,
     date:"comming soon",
     time:"comming soon",
-    venue:"yoga hall or in front of Central Garden ",
-    des: "Participants dress up in characters or object and act or change their accent according to their character.",
+    venue:"YOGA HALL ",
+    des: "Participants dress up as characters or objects and act or modify their accents accordingly.",
     link:"/events/cosplayCompetitionForm",
-    deadline:"comming soon"
+    deadline:"31/01/2025"
   },
   {
     id: 22,
@@ -263,7 +263,7 @@ export const EventsList = [
     venue:"Any classroom",
     des: "Participants have to make an origami with the given papers on the particular themes.",
     link:"/events/origamiForm",
-    deadline:"comming soon"
+    deadline:"31/01/2025"
   }
 
   ];
